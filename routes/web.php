f161c62fb08a7ccd1283702f1a96126270e93853
swo <sergio.sp@sercon.com.br>
2023-08-16 11:07:10 -0300

    Commit 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/supports',[SupportController::class,'index'])->name('Support.index');
 
 Route::get('/', function () {
     return view('welcome');
